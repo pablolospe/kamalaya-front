692017201877b644f76a7125b4a707a89c5dc5d3
pablolospe <yosoypxl@gmail.com>
2023-08-23 11:39:30 -0300

    limpieza

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
-import Image from 'next/image'
-import Header from '@/components/Header'
-import Sidebar from '@/components/Sidebar'
 
 export default function Home() {
   return (
     <main className="m-80">
       <h1>HOME!!!!!</h1>
-     {/* <Sidebar>
-      <Header />
-     </Sidebar> */}
+   
     </main>
   )
 }
