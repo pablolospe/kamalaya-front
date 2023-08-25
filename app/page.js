feec1e791e3bf378c84eced032ca06cdb8825919
pablolospe <yosoypxl@gmail.com>
2023-08-24 22:19:31 -0300

    iconos en home

--- app/page.js
+++ app/page.js
@@ -1,16 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { LuUsers, LuUserPlus } from 'react-icons/lu';
 export default function Home() {
   return (
     <main className="mt-20 flex flex-row justify-evenly align-center">
       <div className="flex flex-col gap-10 justify-center">
 
-        <Link href="/voluntarios" className="w-40 py-2 px-4 bg-gray-200 rounded-md hover:bg-gray-300 shadow-2xl">
-          Ver todos los voluntarios
+        <Link href="/voluntarios" className="flex flex-row items-center w-40 py-2 px-4 bg-gray-200 rounded-md hover:bg-gray-300 shadow-2xl">
+          <span>Ver todos los voluntarios</span>
+          <LuUsers size={40} />
         </Link>
 
-        <Link href="/ingreso" className="w-40 py-2 px-4 bg-gray-200 rounded-md hover:bg-gray-300 shadow-2xl">
-          Ingresar un nuevo voluntario
+        <Link href="/ingreso" className="flex flex-row items-center w-40 py-2 px-4 bg-gray-200 rounded-md hover:bg-gray-300 shadow-2xl">
+          <span>Ingresar un nuevo voluntario</span>
+          <LuUserPlus size={45} />
         </Link>
 
         {/* <button className="w-40 py-2 px-4 bg-gray-200 rounded-md hover:bg-gray-300">
