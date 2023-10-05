ef79f58ae2053b84c78ba79479874b59c804862e
pablolospe <yosoypxl@gmail.com>
2023-10-05 11:37:41 -0300

    no anda el middleware en produccion

--- middleware.ts
+++ middleware.ts
@@ -22,6 +22,6 @@ export async function middleware(req: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: [ '/voluntarios/:path*', '/pacientes/:path*', '/ingreso', '/ingreso/paciente'],
-//   matcher: ['/mapa'],
+//   matcher: [ '/voluntarios/:path*', '/pacientes/:path*', '/ingreso', '/ingreso/paciente'],
+  matcher: ['/mapa'],
 };
