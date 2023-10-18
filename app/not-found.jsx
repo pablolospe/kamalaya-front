96f84e435014165c9e7c538268522828ac71baf3
pablolospe <yosoypxl@gmail.com>
2023-10-18 02:21:20 -0300

    page not found 404

---
+++ app/not-found.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from '@/components/Sidebar';
+
+function NotFound() {
+  return (<>
+  <Sidebar />
+  <main className="mt-80 text-center">Page not-found</main>
+  </>
+  )
+  
+}
+
+export default NotFound;
