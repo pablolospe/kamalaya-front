229e49e8d24574305af36dd77183cf90396d513a
pablolospe <yosoypxl@gmail.com>
2023-12-27 13:47:53 -0300

    lf0

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-// const URL: string = 'https://kamalaya-back.3.us-1.fl0.io'
-const URL: string = 'http://localhost:8000'
+const URL: string = 'https://kamalaya-back.3.us-1.fl0.io'
+// const URL: string = 'http://localhost:8000'
 
 export { URL }
