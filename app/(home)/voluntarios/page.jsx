01f0235a7cb93a6e32136ea6de6826f192883af7
pablolospe <yosoypxl@gmail.com>
2024-01-17 06:12:10 -0300

    details en voluntarios

--- app/(home)/voluntarios/page.jsx
+++ app/(home)/voluntarios/page.jsx
@@ -78,186 +78,188 @@ function Voluntarios() {
   return (
     <div className="flex flex-col gap-2">    
       {/* //////////////// MAPA //////////////// */}
-      <details className={style.details}>
+
+      <details className='bg-gray-100 gap-4 p-4 rounded-lg'>
         <summary className="ml-1 text-md cursor-pointer">Mapa</summary>
-
         <GoogleMapsView marker={voluntariosData} />
-
       </details>
 
       {/* //////////////// FILTROS //////////////// */}
-      <div className="flex flex-wrap flex-col md:flex-row md:justify-evenly md:items-center bg-gray-100 gap-4 p-4 rounded-lg shadow-md">
-        <div>
-          <input
-            name="nombre"
-            type="text"
-            value={query.nombre}
-            onChange={handleChange}
-            className="w-full py-2 px-4 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
-            placeholder="Nombre..."
-          />
-        </div>
-        <div>
-          <input
-            name="apellido"
-            type="text"
-            value={query.apellido}
-            onChange={handleChange}
-            className="w-full py-2 px-4 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
-            placeholder="Apellido..."
-          />
-        </div>
-
-        <div>
-          <input
-            name="localidad"
-            type="text"
-            value={query.localidad}
-            onChange={handleChange}
-            className="w-full py-2 px-4 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
-            placeholder="Localidad..."
-          />
-        </div>
-          
-        <div>
-          <input
-            name="profesion_oficio_ocupacion"
-            type="text"
-            value={query.profesion_oficio_ocupacion}
-            onChange={handleChange}
-            className="w-full py-2 px-4 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
-            placeholder="Profesión / oficio / ocupación..."
-          />
-        </div>
-          
-        <div>
-          <input
-            name="hobbies_habilidades"
-            type="text"
-            value={query.hobbies_habilidades}
-            onChange={handleChange}
-            className="w-full py-2 px-4 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
-            placeholder="hobbies / habilidades..."
-          />
-        </div>
-
-        <div className="bg-white rounded-md shadow-sm p-2 border border-gray-300">
-          <label className="block mb-1 text-gray-400">Disponibilidad</label>
-          <div className="flex flex-wrap gap-x-0.5">
-              <input
-                type="checkbox"
-                value="lunes"
-                checked={query.diaSemana.includes('lunes')}
-                onChange={handleDiaSemanaChange}
-                />
-                <label className='mr-2'>
-                  Lunes</label>
-              <input
-                type="checkbox"
-                value="martes"
-                checked={query.diaSemana.includes('martes')}
-                onChange={handleDiaSemanaChange}
-              />
-            <label className='mr-2'>
-              Martes
-            </label>
-              <input
-                type="checkbox"
-                value="miercoles"
-                checked={query.diaSemana.includes('miercoles')}
-                onChange={handleDiaSemanaChange}
-                />
-            <label className='mr-2'>
-              Miércoles
-            </label>
-              <input
-                type="checkbox"
-                value="jueves"
-                checked={query.diaSemana.includes('jueves')}
-                onChange={handleDiaSemanaChange}
-              />
-            <label className='mr-2'>
-              Jueves
-            </label>
-              <input
-                type="checkbox"
-                value="viernes"
-                checked={query.diaSemana.includes('viernes')}
-                onChange={handleDiaSemanaChange}
-              />
-            <label className='mr-2'>
-              Viernes
-            </label>
-              <input
-                type="checkbox"
-                value="sabado"
-                checked={query.diaSemana.includes('sabado')}
-                onChange={handleDiaSemanaChange}
-              />
-            <label className='mr-2'>
-              Sábado
-            </label>
-              <input
-                type="checkbox"
-                value="domingo"
-                checked={query.diaSemana.includes('domingo')}
-                onChange={handleDiaSemanaChange}
-              />
-            <label className='mr-2'>
-              Domingo
-            </label>
+      <details open className='bg-gray-100 gap-4 p-4 rounded-lg'>
+        <summary className='hover:font-semibold cursor-pointer'>FILTROS</summary>
+        <div className="flex flex-wrap flex-col md:flex-row md:justify-evenly md:items-center bg-gray-100 gap-4 p-4 rounded-lg ">
+          <div>
+            <input
+              name="nombre"
+              type="text"
+              value={query.nombre}
+              onChange={handleChange}
+              className="w-full py-2 px-4 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
+              placeholder="Nombre..."
+            />
           </div>
-        </div>
+          <div>
+            <input
+              name="apellido"
+              type="text"
+              value={query.apellido}
+              onChange={handleChange}
+              className="w-full py-2 px-4 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
+              placeholder="Apellido..."
+            />
+          </div>
 
-        <div className="flex items-center">
-          <label htmlFor="tieneAuto" className="text-xs text-gray-800 mr-2">
-            Tiene Auto?
-          </label>
-          <select
-            name="tieneAuto"
-            value={query.tieneAuto}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300"
-            id="tieneAuto"
+          <div>
+            <input
+              name="localidad"
+              type="text"
+              value={query.localidad}
+              onChange={handleChange}
+              className="w-full py-2 px-4 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
+              placeholder="Localidad..."
+            />
+          </div>
+            
+          <div>
+            <input
+              name="profesion_oficio_ocupacion"
+              type="text"
+              value={query.profesion_oficio_ocupacion}
+              onChange={handleChange}
+              className="w-full py-2 px-4 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
+              placeholder="Profesión / oficio / ocupación..."
+            />
+          </div>
+            
+          <div>
+            <input
+              name="hobbies_habilidades"
+              type="text"
+              value={query.hobbies_habilidades}
+              onChange={handleChange}
+              className="w-full py-2 px-4 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
+              placeholder="hobbies / habilidades..."
+            />
+          </div>
+
+          <div className="bg-white rounded-md shadow-sm p-2 border border-gray-300">
+            <label className="block mb-1 text-gray-400">Disponibilidad</label>
+            <div className="flex flex-wrap gap-x-0.5">
+                <input
+                  type="checkbox"
+                  value="lunes"
+                  checked={query.diaSemana.includes('lunes')}
+                  onChange={handleDiaSemanaChange}
+                  />
+                  <label className='mr-2'>
+                    Lunes</label>
+                <input
+                  type="checkbox"
+                  value="martes"
+                  checked={query.diaSemana.includes('martes')}
+                  onChange={handleDiaSemanaChange}
+                />
+              <label className='mr-2'>
+                Martes
+              </label>
+                <input
+                  type="checkbox"
+                  value="miercoles"
+                  checked={query.diaSemana.includes('miercoles')}
+                  onChange={handleDiaSemanaChange}
+                  />
+              <label className='mr-2'>
+                Miércoles
+              </label>
+                <input
+                  type="checkbox"
+                  value="jueves"
+                  checked={query.diaSemana.includes('jueves')}
+                  onChange={handleDiaSemanaChange}
+                />
+              <label className='mr-2'>
+                Jueves
+              </label>
+                <input
+                  type="checkbox"
+                  value="viernes"
+                  checked={query.diaSemana.includes('viernes')}
+                  onChange={handleDiaSemanaChange}
+                />
+              <label className='mr-2'>
+                Viernes
+              </label>
+                <input
+                  type="checkbox"
+                  value="sabado"
+                  checked={query.diaSemana.includes('sabado')}
+                  onChange={handleDiaSemanaChange}
+                />
+              <label className='mr-2'>
+                Sábado
+              </label>
+                <input
+                  type="checkbox"
+                  value="domingo"
+                  checked={query.diaSemana.includes('domingo')}
+                  onChange={handleDiaSemanaChange}
+                />
+              <label className='mr-2'>
+                Domingo
+              </label>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <label htmlFor="tieneAuto" className="text-xs text-gray-800 mr-2">
+              Tiene Auto?
+            </label>
+            <select
+              name="tieneAuto"
+              value={query.tieneAuto}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300"
+              id="tieneAuto"
+            >
+              <option value="">Todos</option>
+              <option value={true}>Si tiene auto</option>
+              <option value={false}>No tiene auto</option>
+            </select>
+          </div>
+
+          <div className="flex items-center">
+            <label htmlFor="experienciaCP" className="text-xs text-gray-800 mr-2">
+              Tiene experiencia en cuidados paliativos?
+            </label>
+            <select
+              name="experienciaCP"
+              type="text"
+              value={query.experienciaCP}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300"
+              id="experienciaCP"
+            >
+              <option value="">todos</option>
+              <option value={true}>si</option>
+              <option value={false}>no</option>
+            </select>
+          </div>
+
+          <button
+            onClick={handleBorrarFiltros}
+            className="h-14 p-3 text-xs bg-gray-500 text-white rounded-lg hover:bg-gray-600"
+            >
+            Borrar <br></br>filtros
+          </button>
+
+          <button 
+            onClick={() => setShowPacientes(prevShowPacientes => !prevShowPacientes)}
+            className="h-14 p-3 text-xs bg-gray-500 text-white rounded-lg hover:bg-gray-600"
           >
-            <option value="">Todos</option>
-            <option value={true}>Si tiene auto</option>
-            <option value={false}>No tiene auto</option>
-          </select>
+            {!showPacientes ? 'Ver pacientes': 'Ocultar pacientes'}
+          </button>
         </div>
-
-        <div className="flex items-center">
-          <label htmlFor="experienciaCP" className="text-xs text-gray-800 mr-2">
-            Tiene experiencia en cuidados paliativos?
-          </label>
-          <select
-            name="experienciaCP"
-            type="text"
-            value={query.experienciaCP}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300"
-            id="experienciaCP"
-          >
-            <option value="">todos</option>
-            <option value={true}>si</option>
-            <option value={false}>no</option>
-          </select>
-        </div>
-
-        <button
-          onClick={handleBorrarFiltros}
-          className="h-14 p-3 text-xs bg-gray-500 text-white rounded-lg hover:bg-gray-600"
-          >
-          Borrar <br></br>filtros
-        </button>
-
-        <button 
-          onClick={() => setShowPacientes(prevShowPacientes => !prevShowPacientes)}
-          className="h-14 p-3 text-xs bg-gray-500 text-white rounded-lg hover:bg-gray-600"
-        >
-          {!showPacientes ? 'Ver pacientes': 'Ocultar pacientes'}
-        </button>
-      </div>
+      </details>
 
       {/* //////////////// TABLA //////////////// */}
 
