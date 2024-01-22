ee323ff9ceb5a1290c24220d6aa4536a3f727afc
pablolospe <yosoypxl@gmail.com>
2024-01-22 04:52:47 -0300

    orderBy Pacientes

--- app/(home)/pacientes/PacientesPage.jsx
+++ app/(home)/pacientes/PacientesPage.jsx
@@ -16,7 +16,16 @@ function PacientesPage() {
     apellido: '',
     localidad: '',
     hobbies: '',
+    orderBy: '',
   });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setQuery((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -38,12 +47,21 @@ function PacientesPage() {
       <h1 className="text-center font-semibold text-xl">PACIENTES</h1>
 
       <table>
-        <thead>
-          <tr className="bg-gray-100 row-auto">
-            <th className="border p-2">Nombre</th>
-            <th className="hidden md:table-cell border p-2">Teléfono</th>
-          </tr>
-        </thead>
+      <thead>
+        <tr className="bg-gray-100 row-auto">
+          <th className="border p-2">
+            <button name="orderBy" value="nombre" onClick={handleChange}>
+              Nombre ⬇️
+            </button>
+          </th>
+          <th className="border p-2">
+            <button name="orderBy" value="apellido" onClick={handleChange}>
+              Apellido ⬇️
+            </button>
+          </th>
+          <th className="hidden md:table-cell border p-2">Teléfono</th>
+        </tr>
+      </thead>
 
         <tbody>
           {pacientesData?.map((v) => (
@@ -57,7 +75,15 @@ function PacientesPage() {
                     <div className="bg-gray-300 cursor-pointer p-3 mx-2 gap-3 rounded-lg flex flex-row">
                       <LuUser size={20} />
                     </div>
-                    {v.nombre} {v.apellido} ({v.paciente_id})
+                    {v.nombre}
+                  </div>
+                </Link>
+              </td>
+              <td>
+                <Link href={`/pacientes/${v.paciente_id}`}>
+                  <div className="flex flex-row items-center ml-3 my-1 text-left">
+                   
+                    {v.apellido} 
                   </div>
                 </Link>
               </td>
