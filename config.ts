492fd7382f9974f209df4f4089ef28d107255873
pablolospe <yosoypxl@gmail.com>
2024-05-17 12:12:10 -0300

    apuntando a render

--- config.ts
+++ config.ts
@@ -1,4 +1,8 @@
-const URL: string = 'https://kamalaya-back.3.us-1.fl0.io'
+// const URL: string = 'https://kamalaya-back.3.us-1.fl0.io'
+const URL: string = 'https://kamalaya-back3.onrender.com'
 // const URL: string = 'http://localhost:8000'
+
+
+
 
 export { URL }
