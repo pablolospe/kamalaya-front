5e7c89f17bf6c9cfbdcacc0631edd04c81ff6eee
pablolospe <yosoypxl@gmail.com>
2024-06-09 04:07:22 -0300

    formato Mayus maps

--- components/GoogleMapsView.js
+++ components/GoogleMapsView.js
@@ -1,5 +1,6 @@
 'use client';
 import mapaLayout from '@/app/(home)/mapa/layout';
+import { capitalizeFirstLetterOfEachWord } from '@/utils/formats';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import React from 'react';
 
@@ -32,41 +33,39 @@ function GoogleMapsView({ marker }) {
                   position={{ lat: Number(m?.lat), lng: Number(m?.lng) }}
                   icon={{
                     url: m?.paciente_id ? '/green-pin.png' : '/purple-pin.png',
-                    // scaledSize: new window.google.maps.Size(20, 20),
-                    // labelOrigin: new window.google.maps.Point(30, 10),
                   }}
                   label={{
-                    text: `${m?.nombre} ${m?.apellido}`,
+                    text: capitalizeFirstLetterOfEachWord(`${m?.nombre} ${m?.apellido}`),
                     color: m?.paciente_id ? 'green' : 'purple',
-                    // fontSize: "14px", // you can change the size of the label text here
                   }}
                 > 
                   {m?.nombre} 
                 </Marker>
-                {/* <Marker
-                  position={{ lat: Number(m?.lat), lng: Number(m?.lng) }}
-                  // cursor={`seeh`}
-                  icon={{
-                    labelOrigin: new google.maps.Point(10, -10),
-                    url: m?.paciente_id
-                      ? '/wheel_chair_accessible.png'
-                      : '/favicon-32x32.png',
-                    // scaledSize: !m?.paciente_id && new window.google.maps.Size(50, 50),
-                  }}
-                  // type={'info'}
-                  label={{
-                    text: `${m?.nombre} ${m?.apellido}`,
-                    color: m?.paciente_id ? 'teal' : 'midnightblue',
-                  }}
-                >
-                  {m?.nombre}
-                </Marker> */}
               </section>
             ))}
         </GoogleMap>
       </LoadScript>
     </div>
   );
-}
-
-export default GoogleMapsView;
+  }
+  
+  export default GoogleMapsView;
+  
+  {/* <Marker
+    position={{ lat: Number(m?.lat), lng: Number(m?.lng) }}
+    // cursor={`seeh`}
+    icon={{
+      labelOrigin: new google.maps.Point(10, -10),
+      url: m?.paciente_id
+        ? '/wheel_chair_accessible.png'
+        : '/favicon-32x32.png',
+      // scaledSize: !m?.paciente_id && new window.google.maps.Size(50, 50),
+    }}
+    // type={'info'}
+    label={{
+      text: `${m?.nombre} ${m?.apellido}`,
+      color: m?.paciente_id ? 'teal' : 'midnightblue',
+    }}
+  >
+    {m?.nombre}
+  </Marker> */}
