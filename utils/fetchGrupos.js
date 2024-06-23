534289c7ea0f4e4679b7305d42981ce9b969dd12
pablolospe <yosoypxl@gmail.com>
2024-06-23 11:05:02 -0300

    extractor en Grupos All

--- utils/fetchGrupos.js
+++ utils/fetchGrupos.js
@@ -1,6 +1,6 @@
 import { URL } from '@/config';
 
-export const grupos = async (query) => {
+export const grupos = async (query, token) => {
   const queryString = new URLSearchParams();
 
   for (const key in query) {
@@ -18,7 +18,13 @@ export const grupos = async (query) => {
   const url = `${URL}/grupo${queryString ? `?${queryString}` : ''}`;
 
   try {
-    const response = await fetch(url, { cache: 'no-store' });
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+      cache: 'no-store' });
     if (!response.ok) {
       throw new Error(`Error en la solicitud: ${response.status}`);
     }
@@ -29,3 +35,35 @@ export const grupos = async (query) => {
     return null;
   }
 };
+
+
+
+// export const grupos = async (query) => {
+//   const queryString = new URLSearchParams();
+
+//   for (const key in query) {
+//     if (Array.isArray(query[key])) {
+//       query[key].forEach((value) => {
+//         queryString.append(`${key}[]`, value);
+//       });
+//     } else {
+//       if (query[key] !== '') {
+//         queryString.append(key, query[key]);
+//       }
+//     }
+//   }
+
+//   const url = `${URL}/grupo${queryString ? `?${queryString}` : ''}`;
+
+//   try {
+//     const response = await fetch(url, { cache: 'no-store' });
+//     if (!response.ok) {
+//       throw new Error(`Error en la solicitud: ${response.status}`);
+//     }
+//     return response.json();
+//   } catch (error) {
+//     console.error('Error en la solicitud:', error);
+//     // Puedes retornar un valor por defecto o lanzar una excepción personalizada si lo prefieres.
+//     return null;
+//   }
+// };
