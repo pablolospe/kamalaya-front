cbb08daa7b0d6ae633b48c4e49dcafd679ac3295
pablolospe <yosoypxl@gmail.com>
2024-06-25 14:50:46 -0300

    grupos para user

--- app/(home)/grupos/GruposTabla.jsx
+++ app/(home)/grupos/GruposTabla.jsx
@@ -29,30 +29,46 @@ function GruposTabla() {
   }, [query, session, gruposData]);
 
   return (
+    <>
+      {session?.user?.role === 'Admin' ?
+      <div>
+        <Link href={'/grupos/crear'} className="w-40 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+          Crear nuevo grupo
+        </Link>
+        <br/> <br/>
+      </div>
+        : null
+      }
+
       <section className="flex flex-col gap-2 overflow-auto text-sm">
         <table>
           <thead>
             <tr className="bg-gray-100 row-auto">
               <th className="border p-2" >ID</th>
               <th className="border p-2 bg-green-200" >Paciente</th>
-              <th className="border p-2 bg-purple-200" >Voluntario<br/> 1</th>
-              <th className="border p-2 bg-purple-200" >Voluntario<br/> 2</th>
-              <th className="border p-2 bg-purple-200" >Voluntario<br/> 3</th>
+              <th className="border p-2 bg-purple-200" >Voluntario<br /> 1</th>
+              <th className="border p-2 bg-purple-200" >Voluntario<br /> 2</th>
+              <th className="border p-2 bg-purple-200" >Voluntario<br /> 3</th>
               <th className="border p-2 w-2" >Fecha de inicio</th>
               <th className="border p-2 w-2" >Desde las</th>
               <th className="border p-2 w-2" >Hasta las</th>
               <th className="border p-2 w-2" >Día de la semana</th>
               <th className="border p-2" >Descripción</th>
-              <th className="border p-2 w-2" >Editar</th>
-              <th className="border p-2 w-2" >Borrar</th>
+              {session?.user?.role === 'Admin' ?
+                <>
+                  <th className="border p-2 w-2" >Editar</th>
+                  <th className="border p-2 w-2" >Borrar</th>
+                </>
+                : null
+              }
               <th className="border p-2 w-2" >Activo</th>
             </tr>
           </thead>
 
           <tbody>
             {gruposData?.map((g) => (
-              <tr key={g.grupo_id} 
-              className="text-center bg-gray-100 hover:bg-gray-200"
+              <tr key={g.grupo_id}
+                className="text-center bg-gray-100 hover:bg-gray-200"
               >
                 <td className="table-cell p-2 ">{g.grupo_id}</td>
                 <td className="table-cell p-2 text-green-600">{capitalizeFirstLetterOfEachWord(`${g?.Paciente.nombre} ${g.Paciente.apellido}`)}</td>
@@ -65,17 +81,23 @@ function GruposTabla() {
                 <td className="table-cell p-2">{convertirHora(g.horaFin)}</td>
                 <td className="table-cell p-2">{DiaSemanaEnum[g.diaSemana]}</td>
                 <td className="table-cell p-2 text-xs">{g.descripcion}</td>
-                <td className="table-cell p-2"><Link className='p-2 rounded-full hover:bg-gray-300' href={`/grupos/${g.grupo_id}/editar`}>✏️</Link></td>
-                <td className="table-cell p-2">
-                  <BotonBorrarGrupo  id={g.grupo_id} />
-                </td>
+
+                {session?.user?.role === 'Admin' ?
+                  <>
+                    <td className="table-cell p-2"><Link className='p-2 rounded-full hover:bg-gray-300' href={`/grupos/${g.grupo_id}/editar`}>✏️</Link></td>
+                    <td className="table-cell p-2">
+                      <BotonBorrarGrupo id={g.grupo_id} />
+                    </td>
+                  </>
+                  : null
+                }
                 <td className="table-cell p-2">{g.activo ? '✅' : '❌'}</td>
               </tr>
             ))}
           </tbody>
         </table>
       </section>
-    
+    </>
   );
 }
 
--- app/(home)/grupos/page.jsx
+++ app/(home)/grupos/page.jsx
@@ -5,16 +5,10 @@ import Link from 'next/link'
 async function GruposPage() {
 
   return (
-    <div>
-        <Link href={'/grupos/crear'} className="w-40 m-4 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-        Crear nuevo grupo
-        </Link>
-        <br />
-        <br />
-
+    <>
         <GruposTabla />
         <br />
-    </div>
+    </>
   )
 }
 
